db859c4611b6fd6d9bf2915bafb08535e4b8c6d2
Tony <pina.rossette@comunidad.unam.mx>
2022-03-18 15:39:50 -0600

    Agregue los metodos "planificador" y "memoria"

--- GestorDeProcesos.java
+++ GestorDeProcesos.java
@@ -3,4 +3,12 @@ public class GestorDeProcesos {
     public static void main(String[] args) {
         
     }
+
+    public static void planificador() {
+        
+    }
+
+    public static void memoria() {
+        
+    }
 }
